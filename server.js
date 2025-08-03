3647d0008b0205b27021f9780ff654ea2b0a18c4
kdilshan5712 <kdilshanbandara5712@gmail.com>
2025-08-03 13:43:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = require('./app');
 connectDB();
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   
